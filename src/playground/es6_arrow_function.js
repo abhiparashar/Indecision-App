33a1977d6ea22b4123a7c9ff26f98173cb3d2b48
Abhishek Parashar <abhishek@Abhisheks-MacBook-Pro.local>
2020-04-12 17:42:34 +0530

    classes

---
+++ src/playground/es6_arrow_function.js
@@ -0,0 +1,4 @@
+const getfirstName = (fullName)=>{
+    return fullName.split(' ')[0];
+}
+console.log(getfirstName("mike smith"))
--- src/playground/es6_let_const.js
+++ src/playground/es6_let_const.js
@@ -1,2 +1,18 @@
 var NameVar = 'Martin'
+var NameVar = 'andrew'
 console.log("NameVar", NameVar);
+
+let nameLet = 'Jen'
+nameLet  = 'abhishek'
+console.log('nameLet', nameLet)
+
+//const nameConst = 'Frank'
+//nameConst = 'lucy'
+//console.log('nameConst', nameConst)
+
+function getpetName(){
+    var petName = 'hal'
+    return petName
+}
+getpetName()
+console.log(petName)
