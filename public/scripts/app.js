c9859e92b28c5a62935aa8b0a61ff05239776945
abhiparashar <aparashar103@gmail.com>
2020-04-15 19:55:08 +0530

    counter-example

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -67,48 +67,5 @@ var templateTwo = React.createElement(
     getLocation(user.userLocation)
 );
 
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
-};
-
 var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-    var templateThree = React.createElement(
-        "div",
-        null,
-        React.createElement(
-            "h1",
-            null,
-            "count:",
-            count
-        ),
-        React.createElement(
-            "button",
-            { onClick: addOne },
-            "+1"
-        ),
-        React.createElement(
-            "button",
-            { onClick: minusOne },
-            "-1"
-        ),
-        React.createElement(
-            "button",
-            { onClick: reset },
-            "reset"
-        )
-    );
-    ReactDOM.render(templateThree, appRoot);
-};
-renderCounterApp();
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -36,32 +36,5 @@ const templateTwo = (
     {getLocation(user.userLocation)}
 </div>)
 
-let count = 0
-const addOne = () => {
-    count++;
-    renderCounterApp()
-}
-const minusOne = () =>{
-    count--;
-    renderCounterApp()
-}
-const reset = () =>{
-    count=0;
-    renderCounterApp()
-}
-
 const appRoot = document.getElementById('app')
-
-const renderCounterApp = () =>{
-    const templateThree = (
-        <div>
-            <h1>count:{count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    )
-    ReactDOM.render(templateThree, appRoot)
-}
-renderCounterApp()
-
+ReactDOM.render(template, appRoot)
